672d97fed85625d993cf0cf80bab07b11458770c
Nelson Hsu <nelsonhsu1027@gmail.com>
2022-05-14 00:50:03 +0800

    Add Date&Time picker

--- src/Components/SetBlock.jsx
+++ src/Components/SetBlock.jsx
@@ -1,35 +1,63 @@
-import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
+import {useState} from "react"
+import { Button, Divider, Typography, TextField, Stack } from "@mui/material";
+import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
+import {
+  LocalizationProvider,
+  MobileDateTimePicker,
+  DesktopDateTimePicker,
+} from "@mui/x-date-pickers";
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
-import "./SetBlock.scss"
+import "./SetBlock.scss";
 
-const SetBlock = (props)=>{
-    return(
-<div className="from-to-station">
-          <div className="start-label">
-            <label>
-              起站
-            </label>
-            <Typography variant="h6">
-              臺北
-            </Typography>
-          </div>
+const SetBlock = (props) => {
+  const [dateValue, setdateValue] = useState(new Date());
 
-          <Button className="arrow-btn">
-            <ArrowBackIosNewIcon fontSize='small' /><ArrowForwardIosIcon fontSize='small' /></Button>
 
-          <div className="end-label">
-            <label>
-              迄站
-            </label>
-            <Typography variant="h6">
-              高雄
-            </Typography>
-          </div>
+  return (
+    <div>
+      <div className="from-to-station">
+        <div className="start-label">
+          <label>起站</label>
+          <Typography variant="h6">臺北</Typography>
         </div>
-    )
-    }
 
-export default SetBlock
+        <Button className="arrow-btn">
+          <ArrowBackIosNewIcon fontSize="small" />
+          <ArrowForwardIosIcon fontSize="small" />
+        </Button>
+
+        <div className="end-label">
+          <label>迄站</label>
+          <Typography variant="h6">高雄</Typography>
+        </div>
+      </div>
+      <Divider />
+
+      <div className="date-selector">
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          {/* <MobileDateTimePicker
+            label="For mobile"
+            value={dateValue}
+            onChange={(newValue) => {
+              setdateValue(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          /> */}
+          <DesktopDateTimePicker
+            value={dateValue}
+            onChange={(newValue) => {
+              setdateValue(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+      </div>
+      <Divider sx={{marginTop:"8px"}}/>
+
+    </div>
+  );
+};
+
+export default SetBlock;
