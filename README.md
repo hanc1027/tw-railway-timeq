a53e27302e518b4bcef58c6696eabfa709ab6d34
Nelson Hsu <nelsonhsu1027@gmail.com>
2022-05-14 20:12:15 +0800

    Add ptx-logo

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ### API來源
 - [MOTC Transport API V3](https://ptx.transportdata.tw/MOTC/?urls.primaryName=軌道V3)
+
+### API使用手冊
+- [motc-ptx-api-documentation](https://motc-ptx-api-documentation.gitbook.io/motc-ptx-api-documentation/api-shi-yong)
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Topbar from './Components/Topbar'
 import SetBlock from './Components/SetBlock';
 import ResultBlock from './Components/ResultBlock';
 import MobileSetBlock from './Components/Mobile/SetBlock';
+import ptxLogo from "./assets/PTX_LOGO/PTX_logo.png"
 
 
 import useRWD from './hooks/useRWD'
@@ -19,6 +20,10 @@ function App() {
       <div className="desktop-content">
         <SetBlock />
         <ResultBlock />
+      </div>
+
+      <div className="ptx-logo">
+        開放資料來源<br/><img src={ptxLogo} alt="ptxLogo" style={{width:"150px"}}/>
       </div>
 
     </div>);
