db4188a3ea6b9323fa5ec3108361fecbb0133670
Nelson Hsu <nelsonhsu1027@gmail.com>
2022-05-14 10:48:43 +0800

    SetBlock: get startStation & endStation from redux

--- src/Components/SetBlock.jsx
+++ src/Components/SetBlock.jsx
@@ -9,51 +9,21 @@ import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // redux
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { modeHandler } from "../store/mainSlice";
 
 import "./SetBlock.scss";
 
 const SetBlock = (props) => {
   const [dateValue, setdateValue] = useState(new Date());
-  const [startStation, setstartStation] = useState({
-    StationUID: "TRA-1000",
-    StationID: "1000",
-    StationName: {
-      Zh_tw: "臺北",
-      En: "Taipei",
-    },
-    StationPosition: {
-      PositionLon: 121.51784,
-      PositionLat: 25.04771,
-    },
-    StationAddress: "100230臺北市中正區黎明里北平西路 3 號",
-    StationPhone: "02-23713558",
-    StationClass: "0",
-    StationURL:
-      "http://www.railway.gov.tw/tra-tip-web/tip/tip00H/tipH41/viewStaInfo/1000",
-  });
-  const [endStation, setendStation] = useState({
-    StationUID: "TRA-4400",
-    StationID: "4400",
-    StationName: {
-      Zh_tw: "高雄",
-      En: "Kaohsiung",
-    },
-    StationPosition: {
-      PositionLon: 120.30313,
-      PositionLat: 22.63801,
-    },
-    StationAddress: "80750高雄市三民區港西里建國二路 318 號",
-    StationPhone: "07-2352376",
-    StationClass: "0",
-    StationURL:
-      "http://www.railway.gov.tw/tra-tip-web/tip/tip00H/tipH41/viewStaInfo/4400",
-  });
+  const startStation = useSelector(
+    (state) => state.main.startStation
+  );
+  const endStation = useSelector((state) => state.main.endStation);
 
   const dispatch = useDispatch();
   const SelectCity = (startOrEnd) => {
-    dispatch(modeHandler({ mode: "selectCity", startOrEnd:startOrEnd }));
+    dispatch(modeHandler({ mode: "selectCity", startOrEnd: startOrEnd }));
   };
 
   return (
@@ -64,7 +34,9 @@ const SetBlock = (props) => {
           <Typography
             variant="h6"
             className="station-selector"
-            onClick={()=>{SelectCity("start")}}
+            onClick={() => {
+              SelectCity("start");
+            }}
           >
             {startStation.StationName.Zh_tw}
           </Typography>
@@ -80,7 +52,9 @@ const SetBlock = (props) => {
           <Typography
             variant="h6"
             className="station-selector"
-            onClick={()=>{SelectCity("end")}}
+            onClick={() => {
+              SelectCity("end");
+            }}
           >
             {endStation.StationName.Zh_tw}
           </Typography>
